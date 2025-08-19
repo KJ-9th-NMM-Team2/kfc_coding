f77a3ff75e255618d1b7e775f37e8574b9d4134d
ssumday24 <zx546800@naver.com>
2025-08-19 22:58:17 +0900

    fIx : 디테일페이지 CSS, 세부 디자인
    디테일페이지 CSS, 세부 디자인 수정했습니다.

--- frontend/src/components/FestivalDetailHeroSection.jsx
+++ frontend/src/components/FestivalDetailHeroSection.jsx
@@ -1,22 +1,13 @@
 import { Card } from "react-bootstrap";
 
-export default function FestivalDetailHeroSection({ festival }) {
-  const backgroundStyle = {
-    height: "400px",
-    background: "linear-gradient(135deg, #1e3a8a 0%, #7c3aed 100%)",
-    backgroundImage: `url(${festival.thumbnail_url})`, // 썸네일 이미지 적용
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  };
-
+const FestivalDetailHeroSection= ({ festival })=> {
   return (
     <div>
       <Card className="shadow overflow-hidden">
-        <div
-          className="d-flex align-items-center justify-content-center text-white"
-          style={backgroundStyle} // 위의 backgroundStyle 적용
-        ></div>
+        <img src={festival.thumbnail_url} />
       </Card>
     </div>
   );
 }
+
+export default FestivalDetailHeroSection;
