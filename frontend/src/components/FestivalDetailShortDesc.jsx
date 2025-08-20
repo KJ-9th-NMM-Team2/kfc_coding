be37b6ddeca7fc5dda5ac4ad437bcc7bc5172899
ssumday24 <zx546800@naver.com>
2025-08-20 14:31:27 +0900

    fix : DetailPage CSS

--- frontend/src/components/FestivalDetailShortDesc.jsx
+++ frontend/src/components/FestivalDetailShortDesc.jsx
@@ -1,4 +1,6 @@
-import { Card, Container, Badge } from "react-bootstrap";
+import { Card, Container, Badge, Button } from "react-bootstrap";
+
+import { HeartFill } from "react-bootstrap-icons";
 
 export default function FestivalDetailShortDesc({ festival }) {
   // 날짜 YYYY--MM--DD 형식으로 변환 함수 정의
@@ -28,7 +30,7 @@ export default function FestivalDetailShortDesc({ festival }) {
 
   return (
     <Card className="mb-3">
-      <Card.Body className = "lh-lg fw-bold">
+      <Card.Body className="lh-lg fw-bold">
         {/* 축제간단설명 */}
         <Card.Title className="fs-4 fw-bold">
           {festival.short_description}
@@ -38,25 +40,40 @@ export default function FestivalDetailShortDesc({ festival }) {
         <Card.Text className="fs-2 fw-bold">{festival.name}</Card.Text>
 
         {/* 축제 진행 중 or 축제 종료 */}
-        <Badge bg="danger" className="fs-6 fw-bold mb-4 px-3 py-2">
+        <Badge bg="danger" className="fs-4 fw-bold mb-4 px-3 py-2">
           {leftDays}
         </Badge>
 
         {/* 축제기간 */}
         {festival.start_date && festival.end_date && (
-          <Card.Text className="fw-bold">
+          <Card.Text className="fw-bold fs-5">
             {formatDate(festival.start_date)} ~ {formatDate(festival.end_date)}
           </Card.Text>
         )}
 
         {/* 좋아요, 공유, 조회수*/}
         <Card.Text className="fw-bold">
-          좋아요: {festival.likes || 0} | 공유: {festival.shares || 0} | 조회수:{" "}
-          {festival.views || 0}
+          <div className="d-flex align-items-center">
+            <Button variant="outline-danger" size="md" className="me-3">
+              <HeartFill className="me-1" /> {/* 아이콘 */}
+              {festival.likes || 0}
+            </Button>
+
+            <span className="text-muted me-3">
+              공유: {festival.shares || 0}
+            </span>
+
+            <span className="text-muted">조회수: {festival.views || 0}</span>
+          </div>
         </Card.Text>
 
         {/* 행사 내용 */}
-        <Card.Text className = "pretendard text-secondary" style={{ whiteSpace: 'pre-line' }}>{festival.description}</Card.Text>
+        <Card.Text
+          className="pretendard text-secondary"
+          style={{ whiteSpace: "pre-line" }}
+        >
+          {festival.description}
+        </Card.Text>
       </Card.Body>
     </Card>
   );
